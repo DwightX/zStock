810cf973fdf1b3cbc41ba2fab0d138ba7ff53417
Dwight Carter <bigbrother@dwights-mbp.home>
2021-12-30 00:06:40 -0500

    sign up almost done

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,10 +1,98 @@
 import React from 'react'
+import {useState} from 'react'
+import {Link,useNavigate} from 'react-router-dom'
+import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import { getAuth, createUserWithEmailAndPassword,updateProfile } from "firebase/auth"
+import {db} from '../firebase.config'
+import { setDoc, doc, serverTimestamp} from 'firebase/firestore'
+
+
 
 function Signup() {
+const [showPassword,setShowPassword] = useState(false)
+const [formData,setFormData] = useState({
+    name:'',
+    email:'',
+    password:''
+})
+
+const{name,email,password} = FormData
+
+const navigate = useNavigate()
+const onChange = (e) => {
+    setFormData((prevState) => ({
+        ...prevState,
+        [e.target.id]:e.target.value
+    }))
+}
+const onSubmit = async (e) => {
+    e.preventDefault()
+    try{
+        const auth = getAuth()
+        const userCredential = await createUserWithEmailAndPassword
+        (auth,email,password)
+        const user = userCredential.user
+        updateProfile(auth.currentUser,{
+            displayName:name,
+        })
+        const formDataCopy = {...formData} //copying whats in formData State
+        delete formDataCopy.password //deleting password from formDataCopy object
+        formDataCopy.timestamp = serverTimestamp() // setting timestamp to server timestamp
+        await setDoc(doc(db, 'users',user.uid), formDataCopy) //updating database and add user to user collection
+        navigate('/')
+    }catch(error){
+        console.log(error);
+    }
+}
+
     return (
-        <div>
-            <h1>Signup</h1>
+        <>
+        <div className='pageContainer'>
+            <header>
+                <p className='pageHeader'>Welcome Back!</p>
+            </header>
+            <main>
+                <form onSubmit={onSubmit}>
+
+                <input type="text" className='nameInput' placeholder='Name' id='name'
+                     value={email} onChange={onChange}/>
+
+                <input type="email" className='emailInput' placeholder='Email' id='email'
+                    value={email} onChange={onChange}/>
+
+                    <div className='passwordInputDiv'>
+                        <input type={showPassword ? 'text' : 'password'}
+                        className='passwordInput'
+                        placeholder='Password'
+                        id='password'
+                        value={password}
+                        onChange={onChange}/>
+                        <img src={visibilityIcon} 
+                        alt="show password"
+                        className='showPassword'
+                        onClick={() => setShowPassword((prevState) => !prevState)}
+                        />
+                    </div>
+                    <Link to='/forgot-password' className='forgotPasswordLink'>
+                        Forgot Password
+                    </Link>
+                    <div className='signUpBar'>
+                        <p className='signUpText'>
+                            Sign Up
+                        </p>
+                        <button className='signUpButton'>
+                            <ArrowRightIcon fill='white' width='34px'height="34px"></ArrowRightIcon>
+                        </button>
+                    </div>
+                </form>
+                {/* google auth*/}
+                <Link to="/sign-in" className='registerLink'>
+                    Sign in Instead
+                </Link>
+            </main>
         </div>
+        </>
     )
 }
 
